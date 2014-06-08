f0246c39d2d681a5987c5c90e74e0fb5fbbedc73
matembers <matembers@gmail.com>
2014-06-09 01:10:00 +0200

    Amelioration liste

--- js/app.js
+++ js/app.js
@@ -1,63 +1,67 @@
-var app = angular.module('app', []);
-
-app.run(function($http, $rootScope){
-    $http.get("js/import.json").success(function(response){
-        $rootScope.podcasts = response;
-    }).error(function(){
-
-    });
-});
+var app = angular.module('app', [
+  'ngRoute',
+  'appControllers'
+]);
 
 
-app.config(function($routeProvider){
-    $routeProvider
-        .when('/home', {
-            templateUrl:'partials/home.html',
-            controller: "HomeCtrl"
-        })
-        .when('/:podcast', {
-            templateUrl:'partials/podcast.html',
-            controller: "PodcastCtrl"
-        })
-        .otherwise({redirectTo:'/home'})
-});
-
-function HomeCtrl($scope, $http, $rootScope){
-    $scope.panel = 0;
-    $scope.loader  = false;
-}
-
-function PodcastCtrl($scope, $http, $routeParams, $rootScope){
-    $scope.loader  = false;
-
-    $scope.nom = $rootScope.podcasts[$routeParams.podcast].nom;
-    $scope.feed = "xml.php?url="+$rootScope.podcasts[$routeParams.podcast].feed;
 
 
-    $http.get($scope.feed).then(function(response) {
-        var chapters = [];
-     
-        /*setting up the response*/
-        var podcast = response.data;
-        console.log(podcast);
-        console.log(podcast.channel.item[0].enclosure["@attributes"].url);
-        $scope.episodes = podcast.channel.item;
-        /*chapters = courseDef.course.navigation.chapter;
-     
-        var numOfPages = chapters[chapter].length;
-        var thisChapter = chapters[chapter];
-        var numOfPages = thisChapter.page.length;
-     
-        for (var i = 0; i < numOfPages; i++) {
-          pages.push({
-            name: thisChapter.page[i]
+app.run([ '$http', '$rootScope', 
+    function($http, $rootScope){
+        $http.get("js/import.json").success(function(response){
+            $rootScope.podcasts = response;
+        }).error(function(){
+
+        });
+}]);
+
+app.config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider.
+          when('/home', {
+            templateUrl: 'partials/home.html',
+            controller: 'HomeCtrl'
+          }).
+          when('/:podcast', {
+            templateUrl: 'partials/podcast.html',
+            controller: 'PodcastCtrl'
+          }).
+          otherwise({
+            redirectTo: '/home'
           });
-        }
-     
-        $scope.pages = pages;
-        $scope.chapterName = thisChapter.name;
-        */
+}]);
 
 
-      });
-}
+var appControllers = angular.module('appControllers', []);
+
+
+appControllers.controller('HomeCtrl', ['$scope', '$http', '$rootScope',
+    function ($scope, $http, $rootScope) {
+        $scope.panel = 0;
+        $scope.loader  = false;
+    }
+]);
+
+appControllers.controller('PodcastCtrl', ['$scope', '$http', '$sce','$rootScope', '$routeParams',
+    function($scope, $http, $sce, $rootScope, $routeParams) {
+        $scope.loader  = false;
+        $scope.numLimit  = 10;
+
+        $scope.nom = $rootScope.podcasts[$routeParams.podcast].nom;
+        $scope.feed = "xml.php?url="+$rootScope.podcasts[$routeParams.podcast].feed;
+
+        $http.get($scope.feed).success(function(data){
+
+            var podcast = data;
+            $scope.episodes = podcast.channel.item;
+            angular.forEach($scope.episodes, function(value, key) {
+               $scope.episodes[key].url = $sce.trustAsResourceUrl($scope.episodes[key].enclosure["@attributes"].url);
+             });
+            console.log($scope.episodes);
+
+            $scope.numLimitMax  = $scope.episodes.length;
+        }).error(function(){
+
+        });
+    }
+]);
